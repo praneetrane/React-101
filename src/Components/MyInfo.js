d4d7235de35028d8fe00d8137ecfb7c19e784dd8
Praneet Rane <praneet.rane@gmail.com>
2021-05-15 20:04:29 +0530

    JSX to Javascript and Back

--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -3,11 +3,27 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 function MyInfo(){
+  const firstName="Praneet";
+  const lastName="Rane";
+
+  const date = new Date();
+  const hours= date.getHours()
+  let timeofTheDay;
+
+  if (hours<12){
+    timeofTheDay="Morning";
+  }else if(hours >= 12 && hours<17){
+    timeofTheDay="Afternoon";
+  }else{
+    timeofTheDay="Night";
+  }
     return (
      
       <div>
          <Header/>
-        <h1>Praneet Rane</h1>
+        <h1>Hello I am {" "+ firstName + " " + lastName}</h1>
+        <h1>Hello I am {`${firstName} ${lastName}`} </h1>
+        <h2>Good {timeofTheDay} !</h2>
         <p style={{color: "Purple", fontStyle: "Italic"}}>I am a fullstack .Net developer. <br/> My favourite vacation spots are:</p>
         <ul>
           <li>Andaman</li>
