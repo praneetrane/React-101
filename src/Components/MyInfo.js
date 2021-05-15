9cdcc8ddec2ba011b0a64068599b26a57417d718
Praneet Rane <praneet.rane@gmail.com>
2021-05-15 20:35:05 +0530

    React Inline Styles with the Style Property

--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -6,24 +6,35 @@ function MyInfo(){
   const firstName="Praneet";
   const lastName="Rane";
 
-  const date = new Date();
+  const date = new Date(2021, 5, 15, 20);
   const hours= date.getHours()
   let timeofTheDay;
 
+  const styles={ 
+    //color: "Purple" , 
+    //backgroundColor:"Pink",
+    fontSize:"50px"
+  };
+
   if (hours<12){
     timeofTheDay="Morning";
+    styles.color="Yellow";
   }else if(hours >= 12 && hours<17){
     timeofTheDay="Afternoon";
+    styles.color="Red";
   }else{
     timeofTheDay="Night";
+    styles.color="Pink";
   }
-    return (
-     
+
+ 
+    return (   
       <div>
          <Header/>
         <h1>Hello I am {" "+ firstName + " " + lastName}</h1>
         <h1>Hello I am {`${firstName} ${lastName}`} </h1>
-        <h2>Good {timeofTheDay} !</h2>
+        <h2 style={styles}>Good {timeofTheDay} !</h2>
+        <h2 style={{color:"Yellow",backgroundColor:"Red"}}>Good {timeofTheDay} !</h2>
         <p style={{color: "Purple", fontStyle: "Italic"}}>I am a fullstack .Net developer. <br/> My favourite vacation spots are:</p>
         <ul>
           <li>Andaman</li>
