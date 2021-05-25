719c8e1b406c856516a7e86ba166e600ea3d9ca5
Praneet Rane <praneet.rane@gmail.com>
2021-05-25 06:31:35 +0530

    Added class based component for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,19 @@
 import react from 'react';
-
-function Footer(){
+/* //Functional component
+function Footer() {
     return (
         <footer>This is footer component</footer>
     );
+}
+ */
+
+//----Class based component. Created above function based component into class based component.
+class Footer extends react.Component {
+    render() {
+        return (
+            <footer>This is footer component</footer>
+        );
+    }
 }
 
 export default Footer
